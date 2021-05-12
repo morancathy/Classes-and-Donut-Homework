d821937c9a711b3d1d9cc20c155d9f562e5083e8
Cat <morancathy@gmail.com>
2021-05-11 22:50:42 -0500

    created person class

--- objects.js
+++ objects.js
@@ -17,5 +17,47 @@ class Hamster {
   }
 }
 const Gus = new Hamster('Gus');
-Gus.wheelRun();
-console.log(Gus);
+
+class Person {
+  constructor(){
+    this.name = name;
+    this.age = 0;
+    this.height = 0;
+    this.weight = 0;
+    this.mood = 0;
+    this.hamsters = [];
+    this.bankAccount = 0;
+  }
+  getName() {
+    return this.name;
+  }
+  getAge(){
+    return this.age;
+  }
+  getWeight(){
+    return this.weight;
+  }
+  greet(){
+    console.log("Hello" + this.name + "!");
+  }
+  eat(num){
+    this.weight += num;
+    this.mood += num;
+  }
+  exercise(num){
+    this.weight -= num
+  }
+  ageUp(num){
+    this.age += num;
+    this.height += num;
+    this.weight += num;
+    this.mood -= num;
+    this.bankAccount += 10;
+  }
+  buyHamster(hamster){
+    this.hamsters.push(hamster);
+    this.mood += 10;
+    this.bankAccount -= hamster.getPrice();
+    }
+}
+const Cathy = new Person;
